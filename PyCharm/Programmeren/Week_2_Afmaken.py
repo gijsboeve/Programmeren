2698d403b3caf73f0e4d71f7e9b79d39b7c3a6c8
gijsboeve <g.boeve.1@student.rug.nl>
2020-11-29 15:28:20 +0100

    proberen weer wat te uploaden

--- PyCharm/Programmeren/Week_2_Afmaken.py
+++ PyCharm/Programmeren/Week_2_Afmaken.py
@@ -1,3 +1,9 @@
-print("hello world")
-print("dit is mijn nieuwe laptop")
-print("werk GitHub, testtesttest")
+principal_amount = float(input("How many money you want to use"))
+
+interest_rate = int(input("how much interest do you get?"))
+interest_times_a_year = 4
+amount_of_years = 3
+
+final_amount = principal_amount * (1 + (interest_rate/interest_times_a_year)) ** (interest_times_a_year*amount_of_years)
+
+print(final_amount)
